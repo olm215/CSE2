e318e9a1c43b29d17017fdde38daf50fe9ba4560
olm215 <olm215@lehigh.edu>
2015-09-01 04:00:55 +0000

    uploaded hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+/////////////////////
+//
+//welcome Class Java Program
+//
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+public static void main(String[] args){
+    
+// print the statements
+   System.out.println("  -----------");
+   System.out.println("  | WELCOME |");
+   System.out.println("  -----------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+   System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+   System.out.println("<-O--L--M--2--1--5->");
+   System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /  ");  
+   System.out.println("  v  v  v  v  v  v");
+   System.out.println("This summer I interned at structural design company. It was a great learning experience.");
+   
+   //test/testtest
+}
+}
