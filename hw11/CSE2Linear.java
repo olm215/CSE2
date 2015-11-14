0ee4900978c0a248c4e399310484d8a5e209204c
olm215 <olm215@lehigh.edu>
2015-11-14 03:18:18 +0000

    finished hw11

--- hw11/CSE2Linear.java
+++ hw11/CSE2Linear.java
@@ -27,9 +27,9 @@ public class CSE2Linear{
             if(myScanner.hasNextInt()){                     //check if input is of int form
                 grades[i] = myScanner.nextInt();             //store entered input
                 if(grades[i]>0 && grades[i]<101){              //check if entered input is within acceptable range
-                if(i>0){
-                    if(grades[i] >= grades[i-1] ){
-                        break;
+                if(i>0){                                    //if so, proceed
+                    if(grades[i] >= grades[i-1] ){          //check if the input entered greater than last accepted input
+                        break;                              
                     }
                     else{
                         System.out.print("Please enter a grade that is higher than the last grade entered. Try again: ");   //print prompt
@@ -37,7 +37,7 @@ public class CSE2Linear{
                     }
                   }
                 else{
-                    break;
+                    break;              //if first entered grade, do not need to check if its greater than previous one
                 }  
                 }
                 else{                                       //runs if input was not within acceptable range of 0 to 80
@@ -51,49 +51,81 @@ public class CSE2Linear{
           }              
         }
         
-        System.out.print("15 entered ints for final grades in CSE2: ");    //print prompt to try again
-        printArray(grades);
-        scrambled(grades);
-        System.out.print("Scrambled array: ");    //print prompt to try again
-        printArray(grades);
-        System.out.print("Enter a grade to search for: ");
-        int value=myScanner.nextInt();              //input value
-        linearSearch(grades,1 );
-    }
-    public static void printArray(int[] grades){
-        for(int i = 0; i<15; i++){
-        System.out.print(grades[i]+" ");
-        }
-        System.out.println();
-    }
-    
-    public static void searchFor(int[] grades, int value){
+        System.out.print("15 entered ints for final grades in CSE2: ");    //print prompt
+        printArray(grades);                                                //call method to print out an array with grades listed                                    
+        System.out.print("Enter a grade to binary search for: ");          //print prompt
+        int binsearch = myScanner.nextInt();                               //accept the term to be binary searched for
+        binarySearch(grades, binsearch);                                   //call a method binary search            
         
+        scrambled(grades);                          //call method to scramble the array of grades
+        System.out.print("Scrambled array: ");      //print prompt
+        printArray(grades);                         //print the scramble array
+        
+        System.out.print("Enter a grade to linear search for: ");   //print prompt
+        int linsearch = myScanner.nextInt();                        //accept the term to be linearly searched for
+        linearSearch(grades, linsearch);                            //call a method linear search
+
+    }
+    public static void printArray(int[] grades){            //method that prints an array of grades
+        for(int i = 0; i<15; i++){                          //loop to go through all the grades
+        System.out.print(grades[i]+" ");                    //print grade
+        }
+        System.out.println();                               //print line
     }
     
-    public static void scrambled(int[] grades){  //method that gives outs a hand of 5 random cards
+    public static void scrambled(int[] grades){  //method that randomizes the grades within an array
       int rand = 0;                              //initialize an int variable to generate a random number
-      int placeHolder = 0;                 //initialize random string variable to hold the value of card in
-      int[] temp = new int[15];             //initialize string of 5 elements to store cards in
-      for(int i=0; i<15; i++){                    //for loop used to generate 5 cards
-       rand = (int) (Math.random()*15);        //generate a random number from 1 to 52
-       placeHolder = temp[rand];                //put a card at array rand into a string placeHolder
-       temp[i] =  grades[rand];                   //saves random string into hand array
+      int placeHolder = 0;                      //initialize random string variable to hold the value of grade in
+      for(int i=0; i<15; i++){                  //for loop used to generate randomization process
+       rand = (int) (Math.random()*15);        //generate a random number from 0 to 14
+       placeHolder = grades[i];                //put a grade at array grades into a string placeHolder
+       grades[i] =  grades[rand];              //saves random string into grade array
+       grades[rand] = placeHolder;             //places the previous number at location i back into array at position of other grade pulled out
        }                          
     }
     
-    public static void linearSearch(int [] grades, int value) {
-        int iteration = 1; // delcaring and assigning 1 to iteration
-        int i;
-        for (i = 0; i < 15; i++) { // while i is less than 15 this loop will run
-            if (grades[i] == value) { // if val is equal to array sub i this code will run
-                System.out.println(value + " was found in the list with "+iteration+" iterations"); // prints to user
-                break; // exits loop
+    public static void binarySearch(int [] grades, int binsearch) {         //method for binary search
+        int comparisons = 0;                //initialize a variable to count number of comparisons 
+        int min = 0;                        //initialize variable to keep count of min value
+        int max = 14;                       //initialize variable to keep count of max vlaue
+        int mid = (min+max)/2;              //calculate mid value
+        for(int i = 0; i<15; i++){          //loop that helps with tracking number of comparisons made
+            comparisons = i + 1;            //define comparisons
+            if(max >= min){                 //run when max value larger than min
+                mid = (min+max)/2;          //recalculate mid vlaue
+                if(grades[mid]>binsearch){  //determine whether the array grades at location mid is larger than the searched term
+                    max = mid - 1;          //if so, decrease max value by 1
+                }
+                else if(grades[mid]==binsearch){    //determine whether the array grades at location mid is matched with the searched term    
+                    System.out.println("" + binsearch + " was found in the list with " + comparisons + " comparisons"); //if so, print this message out
+                    break;                              //get out of loop
+                }   
+                else{                           //runs when the searched term is greater than the value at an array mid location
+                    min = mid + 1;              //if so, increase min value by 1
+                }
             }
-              iteration++; // increments iteration by 1
+            else{                               //if min value is greater than max, this means the term searched is not in the array
+                System.out.println("" + binsearch + " was not found in the list with " + comparisons + " comparisons");  //print prompt
+                break;                          //get out of the loop
+            }
         }
-        if (grades[i] != value) { // if array sub i never equals val this code will run
-            System.out.println(value + " was not found in the list with " + iteration + " iterations"); // prints to user
+    }
+    
+    public static void linearSearch(int[] grades, int linsearch){  //method for linear search
+        int comparisons = 0;                //initialize a variable to count number of comparisons 
+        int notFound = 0;                   //initialize a variable to count number of comparisons not found
+        for (int i = 0; i<15; i++) {            //loop to go through all grades in an array
+            comparisons = comparisons + 1;
+            if (grades[i] == linsearch) {       //compare i location with user entered search parameter
+               // comparisons = i;                //keeps track of number of comparisons performed
+                System.out.println("" + linsearch + " was found in the list with " + comparisons + " comparisons");      //print prompt
+            }
+            else {
+                notFound++;             //keeps track of not founded comparisons
+            }
+        }
+        if (notFound == 15) {                       //runs when no comparisons were found
+            System.out.println("" + linsearch + " was not found in the list with " + notFound + " comparisons");        //print prompt
         }
     }    
     
--- lab11/lab11Search.java
+++ lab11/lab11Search.java
@@ -12,30 +12,53 @@ import java.util.Scanner;     //import class to accept user input
 // Main program Check begins here
 //
 
-public class lab10Arrays{
+public class lab11Search{
     //main method required for every Java program
     public static void main(String[] args) {
         int[] array1 = new int[5000];        
         int[] array2 = new int[5000];
         Scanner myScanner = new Scanner(System.in);   //allows to prompt user for input
-        for(int = 0; i < 5000; i++){
+        for(int i = 0; i < 5000; i++){
             array1[i] = (int)(Math.random()*100001);
         }
         
-        int min = 0;
-        int max = 0;
+        int min1 = 0;
+        int max1 = 0;
         for(int i=0; i<5000; i++){
-            if(array1[i]>array1[max]){
-                max = i;
+            if(array1[i]>array1[max1]){
+                max1 = i;
             }
-            if(array1[i]<array1[min]){
-                min = i;
+            if(array1[i]<array1[min1]){
+                min1 = i;
             }
         }
         
         for(int i=0; i<5000; i++){
+            array2[i] = (int)(Math.random()*100001);
+            if(i>0) {
+                while(array2[i-2]>=array2[i]){
+                    array2[i] = (int)(Math.random()*100001);
+                }
+            }
+            
             
         }
+        
+        int min2 = 0;
+        int max2 = 0;
+        for(int i=0; i<5000; i++){
+            if(array2[i]>array2[max2]){
+                max2 = i;
+            }
+            if(array2[i]<array2[min2]){
+                min2 = i;
+            }
+        }        
+        
+        System.out.println("The maximum of array1 is: " + max1);
+        System.out.println("The minimum of array1 is: " + min1);
+        System.out.println("The maximum of array2 is: " + max2);
+        System.out.println("The minimum of array2 is: " + min2);        
         
         
         
